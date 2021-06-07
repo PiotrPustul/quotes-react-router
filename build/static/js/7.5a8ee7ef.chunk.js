c0b652eb596fa7c962c86879eb0230d2296755de
pepe199111 <piotrpustul1@gmail.com>
2021-06-07 21:02:57 +0100

    worked on deploying project on firebase

--- build/static/js/7.5a8ee7ef.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonprouter-app"]=this["webpackJsonprouter-app"]||[]).push([[7],{51:function(e,t,n){"use strict";n.r(t);var r=n(1);t.default=function(){return Object(r.jsx)("div",{className:"centered",children:Object(r.jsx)("p",{children:"Page not found"})})}}}]);
-//# sourceMappingURL=7.5a8ee7ef.chunk.js.map
